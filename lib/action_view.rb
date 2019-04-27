dd5105775b56c56e7a0e7e6828ff3ed1c0a97895
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-04-27 08:28:48 +0100

    Initial ActionView implementation
    App can now:
    * Render templates
    * Find a template
    * Render a template with a yield block
    * Render a template with a helper
    
    Added views in Muffin Blog for specs to pass

---
+++ lib/action_view.rb
@@ -0,0 +1,6 @@
+module ActionView
+  autoload :Base, "action_view/base"
+  autoload :Template, "action_view/template"
+  autoload :Helpers, "action_view/helpers"
+  autoload :CompiledTemplates, "action_view/compiled_templates"
+end
---
+++ spec/action_view_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+RSpec.describe ActionView do
+  it 'can render a template' do
+    template = ActionView::Template.new("<p>Hello<p>", "test_render_template")
+    context = ActionView::Base.new
+    expect(template.render(context)).to eq "<p>Hello<p>"
+  end
+
+  it 'can render a template with variables' do
+    template = ActionView::Template.new("<p><%= @var %><p>", "test_render_with_vars")
+    context = ActionView::Base.new var: "var value"
+    expect(template.render(context)).to eq "<p>var value<p>"
+  end
+
+  it 'can render with a yield' do
+    template = ActionView::Template.new("<p><%= yield %><p>", "test_render_with_yield")
+    context = ActionView::Base.new
+    expect(template.render(context) { "yielded content"}).to eq "<p>yielded content<p>"
+  end
+
+  it 'can render with a helper' do
+    template = ActionView::Template.new("<%= link_to 'title', '/url' %>", "test_render_with_helper")
+    context = ActionView::Base.new
+    expect(template.render(context)).to eq "<a href=\"/url\">title</a>"
+  end
+
+  it 'can find a template' do
+    file = "#{__dir__}/muffin_blog/app/views/posts/index.html.erb"
+    template1 = ActionView::Template.find(file)
+    template2 = ActionView::Template.find(file)
+    expect(template2).to eq template1
+  end
+
+end
