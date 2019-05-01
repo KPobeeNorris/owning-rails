4bb3899eba6ca87414a8f6fafb88558248ec3697
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-05-01 18:47:37 +0100

    Add message sending/receiving functionality

--- lib/action_cable/server.rb
+++ lib/action_cable/server.rb
@@ -4,10 +4,15 @@ require "#{Rails.root}/app/channels/application_cable/connection"
 
 module ActionCable
   class Server
-    attr_reader :connections
+    attr_reader :connections, :pubsub
 
     def initialize
       @connections = []
+      @pubsub = SubscriptionAdapter::Inline.new
+    end
+
+    def broadcast(name, data)
+      @pubsub.broadcast(name, data)
     end
 
     def call(env)
--- spec/action_cable_spec.rb
+++ spec/action_cable_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe ActionCable do
     @thread = Thread.new { @server.start }
     wait_for { @server.running? }
 
-    @websocket = Faye::WebSocket::Client.new("ws://127.0.0.1:8082/")
+    @websocket = Faye::WebSocket::Client.new("ws://127.0.0.1:8082/?token=valid")
     wait_for { @websocket.ready_state == Faye::WebSocket::Client::OPEN }
   end
 
@@ -17,9 +17,34 @@ RSpec.describe ActionCable do
     @thread.join
   end
 
-  it 'tests subscribe' do
+  it 'can subscribe to a channel' do
     connection = ActionCable.server.connections.first
     expect(connection)
+    @websocket.send(JSON.dump(command: 'subscribe', channel: 'ChatChannel'))
+
+    wait_for { connection.subscriptions["ChatChannel"] }
+
+    expect(ActionCable.server.pubsub.subscribed?("chat")).to eq true
+  end
+
+  it 'can send and receive messages' do
+    @websocket.send(JSON.dump(command: 'subscribe', channel: 'ChatChannel'))
+
+    received = nil
+    @websocket.on :message do |event|
+      received = JSON.parse(event.data)
+    end
+
+    @websocket.send(JSON.dump(command: 'message', channel: 'ChatChannel', data: "hello!"))
+
+    wait_for { received }
+
+    expect(received).to eq ({ 'channel' => 'ChatChannel', 'message' => 'hello!'})
+  end
+
+  it 'closes the websocket if invalid connection' do
+    @websocket = Faye::WebSocket::Client.new("ws://127.0.0.1:8082/?token=invalid")
+    wait_for { @websocket.ready_state == Faye::WebSocket::Client::CLOSED }
   end
 
   def wait_for
